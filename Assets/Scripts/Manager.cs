77495d06de7c6605ab478f0a2e85267a9653c35f
ZidaneZul <2100498A@student.tp.edu.sg>
2023-01-09 13:19:19 +0800

    made panel for shops
    i comment somethings out in the manager, can delete if not needed.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -27,19 +27,17 @@ public class Manager : MonoBehaviour
     }
     public void SpawnBaby()
     {
-<<<<<<< Updated upstream:Assets/Scripts/Manager.cs
         GameObject gO = (GameObject)Instantiate(Baby, SpawnPoint.position, Quaternion.identity);
         Crawl crawl = gO.GetComponent<Crawl>();
         crawl.moveSpot = BabyMove;
         statText.text = "Int: " + gO.GetComponent<BabyManager>().intelligence + " / " + "Cha: " + gO.GetComponent<BabyManager>().charisma + " / " + "Health: " + gO.GetComponent<BabyManager>().health;
         wBaby.SetActive(true);
-=======
-        GameObject gO = Instantiate(Baby, SpawnPoint);
-        statText.text = "Int: " + gO.GetComponent<BabyManager>().intelligence + " / " + 
-            "Cha: " + gO.GetComponent<BabyManager>().charisma + " / " + 
-            "Health: " + gO.GetComponent<BabyManager>().health;
-        window.SetActive(true);
->>>>>>> Stashed changes:Assets/Scripts/Ui.cs
+
+        //GameObject gO = Instantiate(Baby, SpawnPoint);
+        //statText.text = "Int: " + gO.GetComponent<BabyManager>().intelligence + " / " + 
+        //    "Cha: " + gO.GetComponent<BabyManager>().charisma + " / " + 
+        //    "Health: " + gO.GetComponent<BabyManager>().health;
+        //window.SetActive(true);
     }
     public void CloseWindow()
     {
